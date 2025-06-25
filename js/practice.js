f56a2d06529c9ee30c6c89a3fe960bb2c4cb41e8
livingstonkolbie <kolbie.livingston@mscoding.org>
2025-06-25 11:22:44 -0600

    practicing

--- js/practice.js
+++ js/practice.js
@@ -7,6 +7,11 @@ let counting = 1;
 let charGender;
 
 
+let charizardBtn = document.getElementById("charizardGif");
+charizardBtn.addEventListener("click", showCharizard);
+let charizardBox = document.getElementById("charizardBox");
+let charizard;
+
 async function fetchGender(countATM) {
 
 
@@ -21,7 +26,7 @@ async function fetchGender(countATM) {
         .catch((error) => {
             console.log(error);
         })
-}
+};
 
 async function showNameAndGender() {
 
@@ -30,12 +35,37 @@ async function showNameAndGender() {
     counting++;
 
     // this method BELOW will replace the innerText area each time
-    genderBox.innerText = charGender.gender;
+    // genderBox.innerText = charGender.gender;
 
     // this method BELOW will ADD onto the last thing- make a list
-    // let para = document.createElement("p");
-    // para.innerText = charGender.gender;
+    let para = document.createElement("p");
+    para.innerText = charGender.gender;
 
-    // genderBox.appendChild(para);
+    genderBox.appendChild(para);
 
+};
+
+function getCharizard() {
+    fetch("https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/versions/generation-i/red-blue/6.png")
+    .then(res => res.json())
+    .then(data => {
+        charizard = data;
+    })
+    .catch((error) => {
+        console.log(error)
+    })
 }
+
+
+function showCharizard() {
+
+    getCharizard();
+
+    // charizardBox.innerText = charizard;
+    let charizardPara = document.createElement("img");
+    charizardPara.innerText = charizard;
+
+    charizardBox.appendChild(charizardPara);
+}
+
+// ABOVE i did this all wrong lol... come back to getCharizard() and showCharizard() functions later...
